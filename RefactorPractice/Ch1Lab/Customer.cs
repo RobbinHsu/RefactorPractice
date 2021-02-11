1cadd3f6e713f794f7bbc4f491ec950c6536c4c1
Robbinhs <robbinhsu@gmail.com>
2021-02-11 14:35:05 +0800

    Extract Method
    Rename

--- RefactorPractice/Ch1Lab/Customer.cs
+++ RefactorPractice/Ch1Lab/Customer.cs
@@ -30,49 +30,56 @@ namespace RefactorPractice.Ch1Lab
             var frequentRenterPoints = 0;
             var result = $"Rental Record for {Name} \n";
 
-            _rental.ForEach(
-                item =>
+            foreach (var item in _rental)
+            {
+                var thisAmount = AmountFor(item);
+
+                frequentRenterPoints++;
+                if (item.Movie.PriceCode == Movie.New_Release && item.DayRented > 1)
                 {
-                    var thisAmount = 0.0;
-
-                    switch (item.Movie.PriceCode)
-                    {
-                        case Movie.Regular:
-                            thisAmount += 2;
-                            if (item.DayRented > 2)
-                            {
-                                thisAmount += (item.DayRented - 2) * 1.5;
-                            }
-
-                            break;
-
-                        case Movie.New_Release:
-                            thisAmount += item.DayRented * 3;
-                            break;
-
-                        case Movie.Childrens:
-                            thisAmount += 1.5;
-                            if (item.DayRented > 3)
-                            {
-                                thisAmount += (item.DayRented - 3) * 1.5;
-                            }
-
-                            break;
-                    }
-
                     frequentRenterPoints++;
-                    if (item.Movie.PriceCode == Movie.New_Release && item.DayRented > 1)
-                    {
-                        frequentRenterPoints++;
-                    }
+                }
 
-                    result += $"{item.Movie.Title} {thisAmount} \n";
-                    totalAmount += thisAmount;
-                });
+                result += $"{item.Movie.Title} {thisAmount} \n";
+                totalAmount += thisAmount;
+            }
+
             result += $"Amount owed is {totalAmount} \n";
             result += $"You earned {frequentRenterPoints} frequent renter points";
 
             return result;
+        }
+
+        private double AmountFor(Rental aRental)
+        {
+            var thisAmount = 0.0;
+
+            switch (aRental.Movie.PriceCode)
+            {
+                case Movie.Regular:
+                    thisAmount += 2;
+                    if (aRental.DayRented > 2)
+                    {
+                        thisAmount += (aRental.DayRented - 2) * 1.5;
+                    }
+
+                    break;
+
+                case Movie.New_Release:
+                    thisAmount += aRental.DayRented * 3;
+                    break;
+
+                case Movie.Childrens:
+                    thisAmount += 1.5;
+                    if (aRental.DayRented > 3)
+                    {
+                        thisAmount += (aRental.DayRented - 3) * 1.5;
+                    }
+
+                    break;
+            }
+
+            return thisAmount;
         }
     }
 }
