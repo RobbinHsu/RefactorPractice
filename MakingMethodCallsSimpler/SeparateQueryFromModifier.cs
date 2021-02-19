fd0de9016deb28df24dcf3dc9261e4219f24a179
Robbin.hsu <robbin.hsu@collaborate.com.tw>
2021-02-19 14:21:26 +0800

    FoundPerson 只負責找人 FoundMiscreant 只負責通知
    修改 CheckSecurity
    
    分離兩件事情
    
    Rename

--- MakingMethodCallsSimpler/SeparateQueryFromModifier.cs
+++ MakingMethodCallsSimpler/SeparateQueryFromModifier.cs
@@ -6,29 +6,29 @@ namespace MakingMethodCallsSimpler
     {
         public void CheckSecurity(string[] people)
         {
-            var found = FoundMiscreant(people);
+            SendAlert(people);
+            var found = FoundPerson(people);
             SomeLaterCode(found);
         }
 
 
-        public string FoundMiscreant(string[] people)
+        public void SendAlert(string[] people)
         {
             for (var i = 0; i < people.Length; i++)
             {
                 if (people[i].Equals("Don"))
                 {
                     SendAlert();
-                    return FoundPerson(people);
+                    return;
                 }
 
                 if (people[i].Equals("John"))
                 {
                     SendAlert();
-                    return FoundPerson(people);
+                    return;
                 }
             }
 
-            return FoundPerson(people);
         }
 
         public string FoundPerson(string[] people)
@@ -37,13 +37,11 @@ namespace MakingMethodCallsSimpler
             {
                 if (people[i].Equals("Don"))
                 {
-                    SendAlert();
                     return "Don";
                 }
 
                 if (people[i].Equals("John"))
                 {
-                    SendAlert();
                     return "John";
                 }
             }
