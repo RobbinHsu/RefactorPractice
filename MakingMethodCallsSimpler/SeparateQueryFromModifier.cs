0da8d30cef5d9571fa34fde32d9ee71f49af51d2
Robbin.hsu <robbin.hsu@collaborate.com.tw>
2021-02-19 14:05:55 +0800

    Separate Query From Modifier
    sample code

---
+++ MakingMethodCallsSimpler/SeparateQueryFromModifier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MakingMethodCallsSimpler
+{
+    public class SeparateQueryFromModifier
+    {
+        public void CheckSecurity(string[] people)
+        {
+            var found = FoundMiscreant(people);
+            SomeLaterCode(found);
+        }
+
+
+        public string FoundMiscreant(string[] people)
+        {
+            for (var i = 0; i < people.Length; i++)
+            {
+                if (people[i].Equals("Don"))
+                {
+                    SendAlert();
+                    return "Don";
+                }
+
+                if (people[i].Equals("John"))
+                {
+                    SendAlert();
+                    return "John";
+                }
+            }
+
+            return "";
+        }
+
+        private void SomeLaterCode(string found)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SendAlert()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
